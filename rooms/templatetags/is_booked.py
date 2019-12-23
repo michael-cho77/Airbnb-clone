f48973a79f21c8e5144514b6e8a0873033122e42
michael-cho77 <michael_cho77@naver.com>
2019-12-24 01:56:11 +0900

    Create Reservation, Model Managers
    .

--- rooms/templatetags/is_booked.py
+++ rooms/templatetags/is_booked.py
@@ -11,6 +11,8 @@ def is_booked(room, day):
         return
     try:
         date = datetime.datetime(year=day.year, month=day.month, day=day.number)
+        # relation을 이용해서 reservation의 room을 가져와야 하기때문에 reservation__room = room을 선언함 
+        # __를 통한 검색법으로 reservation의 room으로 BookedDay를 찾을 수 있게됨 
         reservation_models.BookedDay.objects.get(day=date, reservation__room=room)
         return True
     except reservation_models.BookedDay.DoesNotExist:
