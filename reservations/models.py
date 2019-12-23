a0eba77913721d9f58f57c261eeb0c382558721a
Youngchan <michael_cho77@naver.com>
2019-12-23 22:54:12 +0900

    BookedDays
    .

--- reservations/models.py
+++ reservations/models.py
@@ -1,6 +1,19 @@
+import datetime
 from django.db import models
 from django.utils import timezone
 from core import models as core_models
+
+
+class BookedDay(core_models.TimeStampedModel):
+    day = models.DateField()
+    reservation = models.ForeignKey("Reservation", on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "Booked Day"
+        verbose_name_plural = "Booked Days"
+
+    def __str__(self):
+        return str(self.day)
 
 
 class Reservation(core_models.TimeStampedModel):
@@ -43,3 +56,23 @@ class Reservation(core_models.TimeStampedModel):
         return now >= self.check_in and now <= self.check_out
 
     is_finished.boolean = True
+
+    def save(self, *args, **kwargs):
+        # 만약 pk가 None라면 (= 생성하려는 모델이 새로운 것)
+        if self.pk is None:
+            start = self.check_in
+            end = self.check_out
+            difference = end - start
+            # start와 end 사이에 booked_day가 있는가 
+            existing_booked_day = BookedDay.objects.filter(
+                day__range=(start, end)
+            ).exists()
+            if not existing_booked_day:
+                # 해당 기간에 예약되어있지 않으면 reservation에 저장 
+                super().save(*args, **kwargs)
+                for i in range(difference.days + 1):
+                    # 시작일부터 i일까지 더한기간을 day에 저장(시작일 15일 예약일 2 day = 17) 
+                    day = start + datetime.timedelta(days=i)
+                    BookedDay.objects.create(day=day, reservation=self)
+                return
+        return super().save(*args, **kwargs)
