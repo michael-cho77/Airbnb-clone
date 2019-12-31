d792329c679a2b965f84211d07d6ea035e5e0260
michael-cho77 <michael_cho77@naver.com>
2019-12-31 23:13:46 +0900

    Reservation Detail(confrim, cancel function)
    reservation이 pending 상태라면 cancel reservation 버튼이 보이게 구현
    
    만약 유저가 해당 room 의 host라면 confirm과 cancel버튼을 보이게 구현
    
    reservation이 pending인 상태에서 만약 reservation.room.host가 로그인된 user이면 예약 confirm 가능
    
    verv는 cancel, confirm과 같은 단어를 받는 역할

--- reservations/views.py
+++ reservations/views.py
@@ -1,4 +1,5 @@
 import datetime
+from django.http import Http404
 from django.views.generic import View
 from django.contrib import messages
 from django.shortcuts import render, redirect, reverse
@@ -30,8 +31,30 @@ def create(request, room, year, month, day):
 
 
 class ReservationDetailView(View):
-    def get(self, pk):
-        try:
-            reservation = models.Reservation.object.get(pk=pk)
-        except :
-            pass
+    def get(self, *args, **kwargs):
+        pk = kwargs.get("pk")
+        reservation = models.Reservation.objects.get_or_none(pk=pk)
+        if not reservation or (
+            reservation.guest != self.request.user
+            and reservation.room.host != self.request.user
+        ):
+            raise Http404()
+        return render(
+            self.request, "reservations/detail.html", {"reservation": reservation}
+        )
+
+
+def edit_reservation(request, pk, verb):
+    reservation = models.Reservation.objects.get_or_none(pk=pk)
+    if not reservation or (
+        reservation.guest != request.user and reservation.room.host != request.user
+    ):
+        raise Http404()
+    if verb == "confirm":
+        reservation.status = models.Reservation.STATUS_CONFIRMED
+    elif verb == "cancel":
+        reservation.status = models.Reservation.STATUS_CANCELED
+        models.BookedDay.objects.filter(reservation=reservation).delete()
+    reservation.save()
+    messages.success(request, "Reservation Updated")
+    return redirect(reverse("reservations:detail", kwargs={"pk": reservation.pk}))
