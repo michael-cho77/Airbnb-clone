422113f97b9a29c0b42dbf843e620fd0cc384631
michael-cho77 <michael_cho77@naver.com>
2020-01-03 12:54:17 +0900

    Change language using session and middlesware
    "django.middleware.locale.LocaleMiddleware",
    Tag : transe
    
    django-admin makemessages --locale=languge
    django-admin compilemessages

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 import os
 import requests
+from django.utils import translation
 from django.views.generic import FormView, DetailView, UpdateView
 from django.contrib.auth.views import PasswordChangeView
 from django.urls import reverse_lazy
@@ -8,6 +9,7 @@ from django.core.files.base import ContentFile
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.messages.views import SuccessMessageMixin
+from django.http import HttpResponse
 from . import forms, models, mixins
 from django.contrib import messages
 
@@ -297,3 +299,8 @@ def switch_hosting(request):
     return redirect(reverse("core:home"))
 
 
+def switch_language(request):
+    lang = request.GET.get("lang", None)
+    if lang is not None:
+        request.session[translation.LANGUAGE_SESSION_KEY] = lang
+    return HttpResponse(status=200)
