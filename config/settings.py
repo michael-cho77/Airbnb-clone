2232fec8df46ae19ec867b107584587cef360ab6
Youngchan <michael_cho77@naver.com>
2020-01-09 22:12:55 +0900

    .
    .

--- config/settings.py
+++ config/settings.py
@@ -97,8 +97,8 @@ if DEBUG is False:
             "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
         }
     }
-
 else:
+
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.postgresql",
@@ -116,12 +116,13 @@ else:
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-    "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"
     },
-    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator", },
-    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator", },
-    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator", },
+    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
+    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
+    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
 ]
+
 
 
 # Internationalization
