8d13c33e5eccc7e3b8662fc4bc82c140a2d853e1
Youngchan <michael_cho77@naver.com>
2020-02-03 01:53:13 +0900

    NoneType error fix

--- config/custom_storages.py
+++ config/custom_storages.py
@@ -8,3 +8,4 @@ class StaticStorage(S3Boto3Storage):
 
 class UploadStorage(S3Boto3Storage):
     location = "uploads/"
+    file_overwrite = False
--- lists/templatetags/on_favs.py
+++ lists/templatetags/on_favs.py
@@ -11,4 +11,4 @@ def on_favs(context, room):
     the_list = list_models.List.objects.get_or_none(
         user=user, name="My Favourites Houses"
     )
-    return room in the_list.rooms.all()
+    return room in the_list.rooms.all() 
